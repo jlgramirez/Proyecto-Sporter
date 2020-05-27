bf98030be746ba6f28fa6897b5cf998d2f383476
rodriwito <48282960+rodriwito@users.noreply.github.com>
2020-05-27 16:55:36 +0200

    conexion

--- Documentacion/Codigo_Sporter/Sporter/src/Testing/Conexion.java
+++ Documentacion/Codigo_Sporter/Sporter/src/Testing/Conexion.java
@@ -7,7 +7,7 @@ public class Conexion {
 	private Statement command;
 	
 	public Conexion() throws SQLException {
-		command = DriverManager.getConnection("jdbc:mysql://localhost:3306/spoter", "root", "escorpio99").createStatement();
+		command = DriverManager.getConnection("jdbc:mysql://localhost:3306/spoter", "root", "").createStatement();
 	}
 	public Statement getcommand() {
 		return command;
